07bdf04d7362633140eda4c09d99400c95692d0a
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2026-02-28 15:48:55 +0530

    Add files via upload

---
+++ Week1/Day1/FirstProgram.java
@@ -0,0 +1,13 @@
+package org.testleaf.week1.day1;
+
+public class FirstProgram {
+
+    //Declare the main method
+   public static void main(String args[]){
+        System.out.println("Hello Bharath");
+    }
+}
+
+
+
+
---
+++ Week1/Day1/LearnDatatypes.java
@@ -0,0 +1,54 @@
+package org.testleaf.week1.day1;
+
+public class LearnDatatypes {
+
+    //Main Method
+    public static void main(String[] args) {
+
+       
+       byte age=127;  // -128 to 127
+       System.out.println(age);
+
+       //Short  -32000 to 32000
+       short amount=30000;
+
+       //int
+       int amount1=50000;
+       //9,8,7,6
+
+       long mobile=8925411170l;
+
+       //float, double - decimal
+       float rate=10.25f;
+
+       
+       float rupee1=112.3456789f;
+       System.out.println(rupee1);
+       double rupee2=112.3456789;
+       System.out.println(rupee2);
+
+     //char - A single char -number, aphabet, special character
+     char logo='T';
+
+     //boolean - true/false
+     //Condition satisfies -ture
+     //Not satiesfies -false
+
+     boolean doYouHadBreakfast=false;
+     System.out.println(doYouHadBreakfast);
+
+     //String - Collection of Characters
+     // Vineeth-Numbers, Aplabets, specials characters, Combinations
+     String name="Vineeth Babu";
+     //1 Variable
+     System.out.println(name);
+
+     //More than 1 Variable in 1 syso
+     System.out.println(name+" "+age);   //Vineeth127
+
+
+    }
+
+
+}
+
